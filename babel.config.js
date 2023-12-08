43314c69f29932c6e30802d712c2aa01a1e3c606
mdchad <irsyad.muhd@gmail.com>
2023-12-08 10:48:51 +0800

    remove plugin

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = function (api) {
 
   plugins.push('expo-router/babel');
 
-  plugins.push('react-native-reanimated/plugin');
   return {
     presets: ['babel-preset-expo'],
     plugins,
