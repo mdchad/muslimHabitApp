bc3be82eac03224f6fa9404457715c5998990a59
mdchad <irsyad.muhd@gmail.com>
2023-12-22 12:48:35 +0800

    remove package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,54 @@
-import * as Application from 'expo-application';
-import * as Crypto from 'expo-crypto';
-import { Platform } from 'expo-modules-core';
+// import * as Application from 'expo-application';
+// import * as Crypto from 'expo-crypto';
+// import { Platform } from 'expo-modules-core';
 import { Stack } from 'expo-router';
-import * as SecureStore from 'expo-secure-store';
+// import * as SecureStore from 'expo-secure-store';
 import { useEffect } from 'react';
 
 import { RevenueCatProvider } from './providers/RevenueCatProvider';
-import { supabase } from '../utils/supabase';
+// import { supabase } from '../utils/supabase';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-const getDeviceId = async () => {
-  if (Platform.OS === 'android') {
-    return Application.androidId;
-  } else {
-    let deviceId = await SecureStore.getItemAsync('deviceId');
-
-    if (!deviceId) {
-      deviceId = Crypto.randomUUID(); //or generate uuid
-      await SecureStore.setItemAsync('deviceId', deviceId);
-    }
-
-    console.log(deviceId);
-    return deviceId;
-  }
-};
-
-async function signUpWithEmail() {
-  const deviceId = await getDeviceId();
-  try {
-    const { data, error } = await supabase.auth.signUp({
-      email: `${deviceId}@muslimhabit.app`,
-      password: Crypto.randomUUID(),
-      options: {
-        data: {
-          guest_user: true,
-          full_name: 'admin',
-        },
-      },
-    });
-
-    console.log(data);
-    if (!data.session) console.log('Already signed up');
-  } catch (e) {
-    console.log('error', e);
-  }
-}
+// const getDeviceId = async () => {
+//   if (Platform.OS === 'android') {
+//     return Application.androidId;
+//   } else {
+//     let deviceId = await SecureStore.getItemAsync('deviceId');
+//
+//     if (!deviceId) {
+//       deviceId = Crypto.randomUUID(); //or generate uuid
+//       await SecureStore.setItemAsync('deviceId', deviceId);
+//     }
+//
+//     console.log(deviceId);
+//     return deviceId;
+//   }
+// };
+//
+// async function signUpWithEmail() {
+//   const deviceId = await getDeviceId();
+//   try {
+//     const { data, error } = await supabase.auth.signUp({
+//       email: `${deviceId}@muslimhabit.app`,
+//       password: Crypto.randomUUID(),
+//       options: {
+//         data: {
+//           guest_user: true,
+//           full_name: 'admin',
+//         },
+//       },
+//     });
+//
+//     console.log(data);
+//     if (!data.session) console.log('Already signed up');
+//   } catch (e) {
+//     console.log('error', e);
+//   }
+// }
 
 export default function RootLayout() {
   useEffect(() => {
